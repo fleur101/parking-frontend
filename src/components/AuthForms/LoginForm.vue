27361ff09430c4e37b58db8400cf3f237f2b568a
Zohaib Ahmed Butt <zohaibahmedbutt@gmail.com>
2019-11-12 19:02:23 +0200

    Login: Added Form

--- src/components/AuthForms/LoginForm.vue
+++ src/components/AuthForms/LoginForm.vue
@@ -1,3 +1,36 @@
 <template>
-  <h1>Login</h1>
+  <div class="container">
+    <h1>Login</h1>
+    <form @submit.prevent="requestLogIn">
+      <div class="form-group">
+        <label>Username:</label>
+        <input type="text" class="form-control" required="true" v-model="user.username">
+      </div>
+
+      <div class="form-group">
+        <label>Password:</label>
+        <input type="password" required="true" class="form-control" v-model="user.password">
+      </div>
+
+      <div class="form-group">
+        <button class="btn btn-primary">Log In</button>
+      </div>
+    </form>
+  </div>
 </template>
+
+<script>
+  export default {
+    data(){
+      return {
+        user: {},
+        errors: []
+      }
+    },
+    methods: {
+      requestLogIn(){
+        console.log(this.user.username);
+      }
+    }
+  }
+</script>
