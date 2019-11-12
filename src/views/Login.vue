2849769f38dad14b42f5a8571fff4b63ac17dee0
Zohaib Ahmed Butt <zohaibahmedbutt@gmail.com>
2019-11-12 18:38:12 +0200

    Authentication: Added Routing

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+     <LoginForm />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LoginForm from "@/components/AuthForms/LoginForm.vue";
+
+export default {
+  name: "login",
+  components: {
+    LoginForm
+  }
+};
+</script>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="signup">
+     <SignUpForm />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SignUpForm from "@/components/AuthForms/SignUpForm.vue";
+
+export default {
+  name: "signup",
+  components: {
+    SignUpForm
+  }
+};
+</script>
