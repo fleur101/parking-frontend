4639a451dd4cbeb772cee3aa35afd66d7a69c935
Jazib Sawar <jazibsawar@gmail.com>
2019-11-17 01:08:24 +0200

    Vuex(user): implemented authentication

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,4 +1,27 @@
 export default {
   namespaced: true,
-  state: {}
+  state: {
+    user: {}
+  },
+  actions: {
+    async authenticate({ commit }, user) {
+      try {
+        const { data } = await this.$axios.post("/login", user);
+        commit("addUser", data.data);
+        localStorage.setItem("jwt-token", data.token);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  },
+  mutations: {
+    addUser(state, user) {
+      state.user = user;
+    }
+  },
+  getters: {
+    isAuthenticated: () => !!localStorage.getItem("jwt-token"),
+    jwt: () => localStorage.getItem("jwt-token"),
+    get: state => state.user
+  }
 };
