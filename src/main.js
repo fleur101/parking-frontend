c677e3c89e3b4cdb63e89f2214327354fb2b6221
Jazib Sawar <jazibsawar@gmail.com>
2019-11-17 01:09:32 +0200

    Vue: added global axios instance & baseurl

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 import Vue from "vue";
+import axios from "axios";
+import Vuex from "vuex";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import Toasted from 'vue-toasted';
+import Toasted from "vue-toasted";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.VUE_APP_API_URL || "http://localhost:4000/api/v1"
+});
+
+Vue.prototype.$axios = axiosInstance;
+Vuex.Store.prototype.$axios = axiosInstance;
 
 Vue.config.productionTip = false;
 
-Vue.use(Toasted)
+Vue.use(Toasted);
 
 new Vue({
   router,
